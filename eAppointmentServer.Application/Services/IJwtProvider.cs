23a923d313c21ded379c0a50a0e15959e86a1322
Serkan Öztürk <serkanoztrk237@gmail.com>
2024-07-14 17:46:33 +0300

    Prepared Domain and Application layers

---
+++ eAppointmentServer.Application/Services/IJwtProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eAppointmentServer.Domain.Entities;
+
+namespace eAppointmentServer.Application.Services
+{
+    public interface IJwtProvider
+    {
+        string CreateToken(AppUser user);
+    }
+}
